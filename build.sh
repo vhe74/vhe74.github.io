cd1be662ce50d107b49ff86d9e0bd59f844f02ca
Vincent Heuschling <vhe74@affini-tech.com>
2021-07-11 21:18:31 +0200

    installing hugo

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Installing Hugo and Building site "
+
+sudo apt-get install -y git
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.4/hugo_0.84.4_Linux-64bit.deb
+sudo dpkg -i hugo_0.79.0_Linux-64bit.deb
+sudo apt-get install -f
+hugo version
